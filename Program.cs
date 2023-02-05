8935bd953f4a4c014a76968ac2b127a5a6fddbdb
Alexander Daurskikh <sanya219@gmail.com>
2023-02-05 10:03:16 +0100

    Task 25 solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+int Power(int baseNumber, int pow)
+{
+    int result = 1;
+    for(int i = 1; i <= pow; i++)
+        result *= baseNumber;
+    return result;
+    
+}
+
+Console.Write("Please, enter base: ");
+int baseNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please, enter power: ");
+int powNum = Convert.ToInt32(Console.ReadLine());
+
+int result = Power(baseNum, powNum);
+Console.WriteLine($"{baseNum} raised to the power of {powNum} is {result}.");
