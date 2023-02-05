8b782bc696e0dccd0b61821bb7fc475239b01218
Alexander Daurskikh <sanya219@gmail.com>
2023-02-05 10:21:40 +0100

    Task 29 solved

--- Program.cs
+++ Program.cs
@@ -46,3 +46,25 @@ Console.WriteLine($"The summ of digits in the number {number} is {sum}"); */
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter array[{i}]: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.Write("\n");
+}
+
+Console.Write("Enter array size: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(arraySize);
+PrintArray(array);
