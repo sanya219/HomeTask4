a3601d05f619824a819c32c786dc9f8d857c948a
Alexander Daurskikh <sanya219@gmail.com>
2023-02-05 10:12:34 +0100

    Task 27 solved

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
-int Power(int baseNumber, int pow)
+/* int Power(int baseNumber, int pow)
 {
     int result = 1;
     for(int i = 1; i <= pow; i++)
@@ -18,4 +18,31 @@ Console.Write("Please, enter power: ");
 int powNum = Convert.ToInt32(Console.ReadLine());
 
 int result = Power(baseNum, powNum);
-Console.WriteLine($"{baseNum} raised to the power of {powNum} is {result}.");
+Console.WriteLine($"{baseNum} raised to the power of {powNum} is {result}."); */
+/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+/*
+int DigitSum(int number)
+{
+    int sum = 0;
+    while(number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum > 0 ? sum: -sum; // Потому что при отрицательных числах сумма получалась отрицательной (видимо, остаток от деления отрицательный)
+}
+
+Console.Write("Please, enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = DigitSum(number);
+Console.WriteLine($"The summ of digits in the number {number} is {sum}"); */
+/*
+Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
